b5169c4ccd572188067550eb7fbbbb917922f154
Robert M Flight <rflight79@gmail.com>
2018-02-09 10:32:54 +0000

    version 1.0.0

---
+++ R/decision_logic.R
@@ -0,0 +1,138 @@
+#' Progress Output Location
+#'
+#' Provides functionality to decide **how** the progress should be written,
+#' if at all.
+#'
+#' @details
+#'
+#' This function makes decisions about **how** the progress bar should be displayed
+#' based on whether:
+#'
+#' 1. The code is being run in an interactive session or not
+#' 1. The code is part of a `knitr` evaluation using `knit()` or `rmarkdown::render()`
+#' 1. Options set by the user. These options include:
+#'     1. **kpb.suppress_noninteractive**: a logical value. Whether to suppress output
+#'   when being run non-interactively.
+#'     1. **kpb.use_logfile**: logical, should a log-file be used for output?
+#'     1. **kpb.log_file**: character string defining the log-file to use. **kpb.use_logfile** must be `TRUE`.
+#'     1. **kpb.log_pattern**: character string providing a pattern to use, will be combined with the chunk
+#'   label to create a log-file for each knitr chunk. **kpb.use_logfile** must be `TRUE`.
+#'
+#' Based on these, it will either return a newly opened connection, either via
+#' `stderr()`, `stdout()`, or a file connection via `file("logfile.log", open = "w")`.
+#' Note that for files this will overwrite a previously existing file, and the contents
+#' will be lost.
+#'
+#' @examples
+#' \dontrun{
+#' # suppress output when not interactive
+#' options(kpb.suppress_noninteractive = TRUE)
+#'
+#' # use a log-file, will default to kpb_output.txt
+#' options(kpb.use_logfile = TRUE)
+#'
+#' # use a specific log-file
+#' options(kpb.use_logfile = TRUE)
+#' options(kpb.log_file = "progress.txt")
+#'
+#' # use a log-file based on chunk names
+#' options(kpb.use_logfile = TRUE)
+#' options(kpb.log_pattern = "pb_out_")
+#' # for a document with a chunk labeled: "longcalc", this will generate "pb_out_longcalc.log"
+#' }
+#'
+#' @export
+#'
+#' @return a write-able connection or NULL
+make_kpb_output_decisions <- function(){
+
+  all_options <- options()
+  if (is.null(all_options$kpb.suppress_noninteractive)) {
+    suppress_noninteractive <- FALSE
+  } else {
+    suppress_noninteractive <- all_options$kpb.suppress_noninteractive
+  }
+
+  if (is.null(all_options$kpb.use_logfile)) {
+    use_logfile <- FALSE
+  } else {
+    use_logfile <- all_options$kpb.use_logfile
+  }
+
+  # dont worry about suppressing a non-interactive session or using log-files first
+
+  # use stderr to see progress if we are in knitr and not use a log-file
+  if (is_interactive() && is_in_knitr() && !suppress_noninteractive) {
+    pb_connection <- stderr()
+  } else if (!is_interactive() && is_in_knitr() && !suppress_noninteractive) {
+    # regardless of whether interactive or not, because knitr suppresses the output to stdout
+    pb_connection <- stderr()
+  } else if (is_interactive() && !is_in_knitr() && !suppress_noninteractive) {
+    # however, we can use stdout as soon as we are not in knitr itself
+    pb_connection <- stdout()
+  } else if (!is_interactive() && !is_in_knitr() && !suppress_noninteractive) {
+    pb_connection <- stdout()
+  } else if (!is_interactive() && !is_in_knitr() && suppress_noninteractive) {
+    # now address suppressing non-interactive
+    pb_connection <- NULL
+  } else if (!is_interactive() && is_in_knitr() && suppress_noninteractive) {
+    # now address suppressing non-interactive
+    pb_connection <- NULL
+  }
+
+  if (use_logfile) {
+    log_connection <- set_logfile(all_options)
+
+    if (!is.null(pb_connection)) {
+      log_message <- paste0("\nProgress is being logged in: ",
+                            get_con_description(log_connection), "\n")
+      cat(log_message, file = pb_connection)
+    }
+    # replace the progress bar connection with our new one, b/c we are pushing
+    # it to the log file
+    pb_connection <- log_connection
+  }
+
+  pb_connection
+}
+
+get_con_description <- function(con){
+  unlist(summary.connection(con))["description"]
+}
+
+# defining our own version of `interactive` so we can mock it in the tests
+is_interactive <- function() {interactive()}
+
+is_in_knitr <- function() {
+  isTRUE(getOption("knitr.in.progress"))
+}
+
+get_chunk_label <- function() {
+  if (is_in_knitr()){
+    out_label <- knitr::opts_current$get()$label
+  } else {
+    out_label <- ""
+  }
+  out_label
+}
+
+set_logfile <- function(all_options) {
+  if (!is.null(all_options$kpb.log_file)) {
+    logfile <- file(all_options$kpb.log_file, open = "w")
+    attr(logfile, "class") <- "kpblogfile"
+    return(logfile)
+  }
+
+  if (is_in_knitr() && !is.null(all_options$kpb.log_pattern)) {
+    chunk_label <- get_chunk_label()
+    logfile <- file(paste0(all_options$kpb.log_pattern, chunk_label, ".log"), open = "w")
+    attr(logfile, "class") <- "kpblogfile"
+    return(logfile)
+  }
+
+  logfile <- file("kpb_output.log", open = "w")
+  attr(logfile, "class") <- "kpblogfile"
+  logfile
+
+}
+
---
+++ R/progress.R
@@ -0,0 +1,223 @@
+# The original source for much of this came from Hadley Wickham's dplyr
+# code in github.com/tidyverse/dplyr/R/progress.R.
+#
+# Below is the original license statement for the dplyr package.
+#
+# The MIT License (MIT)
+# =====================
+#
+#   Copyright © 2013-2015 RStudio and others.
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the “Software”), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+#   The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+
+# changes to these functions by Robert M Flight
+# 1. addition of progress_location as a field of the Progress object, argument to
+#    initialize, and argument of constructor
+# 2. addition of file argument to cat_line function, and all calls to it
+# 3. updates to documentation due to new functionality
+# 4. removal of decision logic in the Progress$initialize function
+# 5. Addition of Progress$finalizer to close file connections that are given
+
+#' Progress bar with estimated time.
+#'
+#' This provides a reference class representing a text progress bar that displays the
+#' estimated time remaining. When finished, it displays the total duration.  The
+#' automatic progress bar can be disabled by setting `progress_location = NULL`.
+#'
+#' @param n Total number of items
+#' @param min_time Progress bar will wait until at least `min_time`
+#'   seconds have elapsed before displaying any results.
+#' @param progress_location where to write the progress to. Default is to make
+#' decisions based on location type using `make_kpb_output_decisions()`.
+#'
+#' @seealso [make_kpb_output_decisions()]
+#'
+#' @return A ref class with methods `tick()`, `print()`,
+#'   `pause()`, and `stop()`.
+#' @export
+#' @examples
+#' p <- progress_estimated(3)
+#' p$tick()
+#' p$tick()
+#' p$tick()
+#'
+#' p <- progress_estimated(3)
+#' for (i in 1:3) p$pause(0.1)$tick()$print()
+#'
+#' p <- progress_estimated(3)
+#' p$tick()$print()$
+#'  pause(1)$stop()
+#'
+#' # If min_time is set, progress bar not shown until that many
+#' # seconds have elapsed
+#' p <- progress_estimated(3, min_time = 3)
+#' for (i in 1:3) p$pause(0.1)$tick()$print()
+#'
+#' \dontrun{
+#' p <- progress_estimated(10, min_time = 3)
+#' for (i in 1:10) p$pause(0.5)$tick()$print()
+#'
+#' # output to stderr
+#' p <- progress_estimated(10, progress_location = stderr())
+#'
+#' # output to a file
+#' p <- progress_estimated(10, progress_location = tempfile(fileext = ".log"))
+#' }
+#'
+#' @importFrom R.oo abort
+progress_estimated <- function(n, min_time = 0, progress_location = make_kpb_output_decisions()) {
+  if (!inherits(n, c("numeric", "integer"))) {
+    warning("n is not a number, trying to take the length of n ...")
+    n <- length(n)
+  }
+  if (n <= 1) {
+    stop("n must be > 1!")
+  }
+  Progress$new(n, min_time = min_time, progress_location = progress_location)
+}
+
+#' @importFrom R6 R6Class
+Progress <- R6::R6Class("Progress",
+  public = list(
+    n = NULL,
+    i = 0,
+    init_time = NULL,
+    stopped = FALSE,
+    stop_time = NULL,
+    min_time = NULL,
+    last_update = NULL,
+    progress_location = NULL,
+
+    initialize = function(n, min_time = 0, progress_location, ...) {
+      self$progress_location <- progress_location
+      self$n <- n
+      self$min_time <- min_time
+      self$begin()
+    },
+
+    begin = function() {
+      "Initialise timer. Call this before beginning timing."
+      self$i <- 0
+      self$last_update <- self$init_time <- now()
+      self$stopped <- FALSE
+      self
+    },
+
+    pause = function(x) {
+      "Sleep for x seconds. Useful for testing."
+      Sys.sleep(x)
+      self
+    },
+
+    width = function() {
+      getOption("width") - nchar("|100% ~ 99.9 h remaining") - 2
+    },
+
+    tick = function() {
+      "Process one element"
+      if (self$stopped) return(self)
+
+      if (self$i == self$n) abort("No more ticks")
+      self$i <- self$i + 1
+      self
+    },
+
+    stop = function() {
+      if (self$stopped) return(self)
+
+      self$stopped <- TRUE
+      self$stop_time <- now()
+      self
+    },
+
+    print = function(...) {
+      if (is.null(self$progress_location)) {
+        return(invisible(self))
+      }
+
+      if (self$stopped) {
+        overall <- show_time(self$stop_time - self$init_time)
+        if (self$i == self$n) {
+          cat_line(file = self$progress_location, "Completed after ", overall)
+          cat("\n")
+        } else {
+          cat_line("Killed after ", overall)
+          cat("\n")
+        }
+        return(invisible(self))
+      }
+
+      now_ <- now()
+      if (now_ - self$init_time < self$min_time || now_ - self$last_update < 0.05) {
+        return(invisible(self))
+      }
+      self$last_update <- now_
+
+      avg <- (now() - self$init_time) / self$i
+      time_left <- (self$n - self$i) * avg
+      nbars <- trunc(self$i / self$n * self$width())
+
+      cat_line(file = self$progress_location,
+        "|", str_rep("=", nbars), str_rep(" ", self$width() - nbars), "|",
+        format(round(self$i / self$n * 100), width = 3), "% ",
+        "~", show_time(time_left), " remaining"
+      )
+
+      invisible(self)
+    },
+
+    finalize = function() {
+      if (!is.null(self$progress_location)) {
+        if (inherits(self$progress_location, "kpblogfile")) {
+          close.connection(self$progress_location)
+        }
+      }
+
+    }
+
+  )
+)
+
+cat_line <- function(file = NULL, ...) {
+  msg <- paste(..., sep = "", collapse = "")
+  gap <- max(c(0, getOption("width") - nchar(msg, "width")))
+  cat("\r", msg, rep.int(" ", gap), sep = "", file = file, append = TRUE)
+  utils::flush.console()
+}
+
+str_rep <- function(x, i) {
+  paste(rep.int(x, i), collapse = "")
+}
+
+show_time <- function(x) {
+  if (x < 60) {
+    paste(round(x), "s")
+  } else if (x < 60 * 60) {
+    paste(round(x / 60), "m")
+  } else {
+    paste(round(x / (60 * 60)), "h")
+  }
+}
+
+now <- function() proc.time()[[3]]
