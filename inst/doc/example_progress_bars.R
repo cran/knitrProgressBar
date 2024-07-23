6d0b301063c752d5ddce7d936100ac0fe031986e
Robert M Flight <rflight79@gmail.com>
2024-07-23 18:00:01 +0000

    version 1.1.1

--- inst/doc/example_progress_bars.R
+++ inst/doc/example_progress_bars.R
@@ -1,10 +1,10 @@
-## ----init, include = FALSE-----------------------------------------------
+## ----init, include = FALSE----------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----setup---------------------------------------------------------------
+## ----setup--------------------------------------------------------------------
 library(knitrProgressBar)
 
 arduously_long_nchar <- function(input_var, .pb=NULL) {
@@ -17,27 +17,27 @@ arduously_long_nchar <- function(input_var, .pb=NULL) {
   
 }
 
-## ----make_kpb_1, eval = FALSE--------------------------------------------
+## ----make_kpb_1, eval = FALSE-------------------------------------------------
 #  # not run
 #  pb <- progress_estimated(length(letters))
 #  
 #  purrr::map_int(letters, arduously_long_nchar, .pb = pb)
 
-## ----make_kpb_suppression, eval = FALSE----------------------------------
+## ----make_kpb_suppression, eval = FALSE---------------------------------------
 #  options(kpb.suppress_noninteractive = TRUE)
 
-## ----make_kpb_logfiles, eval = FALSE-------------------------------------
+## ----make_kpb_logfiles, eval = FALSE------------------------------------------
 #  options(kpb.use_logfile = TRUE)
 
-## ----make_kpb_specific_file, eval = FALSE--------------------------------
+## ----make_kpb_specific_file, eval = FALSE-------------------------------------
 #  options(kpb.use_logfile = TRUE)
 #  options(kpb.log_file = "my_logfile.log")
 
-## ----make_kkpb_pattern, eval = FALSE-------------------------------------
+## ----make_kkpb_pattern, eval = FALSE------------------------------------------
 #  options(kpb.use_logfile = TRUE)
 #  options(kpb.log_pattern = "pb_out_")
 
-## ----direct_con, eval = FALSE--------------------------------------------
+## ----direct_con, eval = FALSE-------------------------------------------------
 #  # to terminal, or print in a knitr chunk
 #  pb <- progress_estimated(length(letters), progress_location = stdout())
 #  
@@ -47,7 +47,7 @@ arduously_long_nchar <- function(input_var, .pb=NULL) {
 #  # to a file, visible using tailf
 #  pb <- progress_estimated(length(letters), progress_location = file("progress.log", open = "w"))
 
-## ----test_pb_null--------------------------------------------------------
+## ----test_pb_null-------------------------------------------------------------
 pb <- progress_estimated(length(letters), progress_location = NULL)
 
 purrr::map_int(letters, arduously_long_nchar, .pb = pb)
--- inst/doc/multiprocessing.R
+++ inst/doc/multiprocessing.R
@@ -1,10 +1,10 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----setup_mp, eval = FALSE----------------------------------------------
+## ----setup_mp, eval = FALSE---------------------------------------------------
 #  library(knitrProgressBar)
 #  library(parallel)
 #  
@@ -18,14 +18,14 @@ knitr::opts_chunk$set(
 #  
 #  }
 
-## ----set_progress, eval = FALSE------------------------------------------
+## ----set_progress, eval = FALSE-----------------------------------------------
 #  set_kpb <- set_progress_mp("progress_file.log")
 
-## ----run_mp, eval = FALSE------------------------------------------------
+## ----run_mp, eval = FALSE-----------------------------------------------------
 #  options(mc.cores = 2)
 #  mclapply(seq(1, 100), arduously_long_nchar, .pb = set_kpb)
 
-## ----watch_mp, eval = FALSE----------------------------------------------
+## ----watch_mp, eval = FALSE---------------------------------------------------
 #  kpb_watch <- watch_progress_mp(100, watch_location = "progress_file.log")
 #  kpb_watch
 
